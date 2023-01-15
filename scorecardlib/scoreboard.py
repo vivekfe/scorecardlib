3b38a6831cfbffc8c718d28e763dbcbef745bcbd
Vivek Srivastava <435616+vivekfe@users.noreply.github.com>
2023-01-15 12:03:36 +0100

    -add update_score method -test for update_score method. Also validates if there is a match ongoing between given two teams

--- scorecardlib/scoreboard.py
+++ scorecardlib/scoreboard.py
@@ -1,13 +1,29 @@
 from datetime import datetime
 from scorecardlib.game import Game
-from dataclasses import dataclass
-
+from dataclasses import dataclass, field
+from typing import Dict
 @dataclass
 class Scoreboard:
-    games={}
+    games: Dict[str, Game] = field(default_factory=dict)
 
     def start_game(self, home_team: str, away_team: str):
         """Calling this method with team names will add this as
         a key as joins of names and game  instance as value"""
         game = Game(home_team, away_team)
         self.games[f"{home_team}-{away_team}"] = game
+
+    def check_if_match_ongoing(self, home_team, away_team):
+        if not f"{home_team}-{away_team}" in self.games.keys():
+            #validate if the given combination of teams exist in scoreboard
+            raise Exception(f"There is no ongoing game between {home_team} and {away_team}\n")
+        return True
+
+    def update_score(self, home_team: str, away_team: str, home_score: int, away_score: int):
+        """this method updates the score of any running games in scoreboard"""
+        team_combination = f"{home_team}-{away_team}"
+        if self.check_if_match_ongoing(home_team, away_team):
+            self.games[team_combination].home_score= home_score
+            self.games[team_combination].away_score= away_score
+
+    def finish_game(self, home_team, away_team):
+        pass
--- tests/Test_scoreboard_class.py
+++ tests/Test_scoreboard_class.py
@@ -21,3 +21,25 @@ class TestScoreBoard(TestCase):
         leaderboard.start_game("Germany", "France")
         self.assertEqual(leaderboard.games["Germany-France"].home_team, "Germany")
         self.assertEqual(leaderboard.games["Germany-France"].away_team, "France")
+
+    def test_update_Score(self):
+        leaderboard = Scoreboard()
+        home_team = "Germany"
+        away_team= "France"
+        team_combination = f"{home_team}-{away_team}"
+        leaderboard.start_game(home_team, away_team)
+        self.assertEqual(leaderboard.games[team_combination].home_team, "Germany")
+        self.assertEqual(leaderboard.games[team_combination].away_team, "France")
+        leaderboard.update_score(home_team, away_team, 10, 5)
+        self.assertEqual(leaderboard.games[team_combination].home_score, 10)
+        self.assertEqual(leaderboard.games[team_combination].away_score, 5)
+
+    def test_check_if_match_ongoing(self):
+        # Test to check if any team exists
+        leaderboard = Scoreboard()
+        home_team = "Britain"
+        away_team = "Isreal"
+        with self.assertRaises(Exception) as context:
+            leaderboard.update_score(home_team, away_team, 10, 5)
+
+        self.assertTrue(f"There is no ongoing game between {home_team} and {away_team}\n" in str(context.exception))
