2e925f3573d84aaf093b2de7798ae574a16ba058
Vivek Srivastava <435616+vivekfe@users.noreply.github.com>
2023-01-15 12:23:59 +0100

    - test for generating the summary of the games

--- scorecardlib/scoreboard.py
+++ scorecardlib/scoreboard.py
@@ -1,7 +1,8 @@
-from datetime import datetime
 from scorecardlib.game import Game
 from dataclasses import dataclass, field
 from typing import Dict
+
+
 @dataclass
 class Scoreboard:
     games: Dict[str, Game] = field(default_factory=dict)
@@ -13,17 +14,25 @@ class Scoreboard:
         self.games[f"{home_team}-{away_team}"] = game
 
     def check_if_match_ongoing(self, home_team, away_team):
-        if not f"{home_team}-{away_team}" in self.games.keys():
-            #validate if the given combination of teams exist in scoreboard
+        if f"{home_team}-{away_team}" not in self.games.keys():
+            # validate if the given combination of teams exist in scoreboard
             raise Exception(f"There is no ongoing game between {home_team} and {away_team}\n")
         return True
 
     def update_score(self, home_team: str, away_team: str, home_score: int, away_score: int):
         """this method updates the score of any running games in scoreboard"""
-        team_combination = f"{home_team}-{away_team}"
+        match_key = f"{home_team}-{away_team}"
         if self.check_if_match_ongoing(home_team, away_team):
-            self.games[team_combination].home_score= home_score
-            self.games[team_combination].away_score= away_score
+            self.games[match_key].home_score = home_score
+            self.games[match_key].away_score = away_score
 
     def finish_game(self, home_team, away_team):
-        pass
+        if self.check_if_match_ongoing(home_team, away_team):
+            match_key = f"{home_team}-{away_team}"
+            self.games[match_key].finish_game()
+            # finishing a game from leaderboard will trigger  finish_game of the Game class
+            self.games.pop(match_key)
+
+    def get_games_summary(self):
+        sorted_games = sorted(self.games, key= lambda x: (x.total_score(), x.start_tim), reverse=True)
+        return sorted_games
--- tests/Test_scoreboard_class.py
+++ tests/Test_scoreboard_class.py
@@ -1,5 +1,4 @@
 from unittest import TestCase
-from scorecardlib.game import Game
 from scorecardlib.scoreboard import Scoreboard
 
 """Sample table provided as an input and can be used for writing tests as well"""
@@ -25,14 +24,14 @@ class TestScoreBoard(TestCase):
     def test_update_Score(self):
         leaderboard = Scoreboard()
         home_team = "Germany"
-        away_team= "France"
-        team_combination = f"{home_team}-{away_team}"
+        away_team = "France"
+        match_key = f"{home_team}-{away_team}"
         leaderboard.start_game(home_team, away_team)
-        self.assertEqual(leaderboard.games[team_combination].home_team, "Germany")
-        self.assertEqual(leaderboard.games[team_combination].away_team, "France")
+        self.assertEqual(leaderboard.games[match_key].home_team, "Germany")
+        self.assertEqual(leaderboard.games[match_key].away_team, "France")
         leaderboard.update_score(home_team, away_team, 10, 5)
-        self.assertEqual(leaderboard.games[team_combination].home_score, 10)
-        self.assertEqual(leaderboard.games[team_combination].away_score, 5)
+        self.assertEqual(leaderboard.games[match_key].home_score, 10)
+        self.assertEqual(leaderboard.games[match_key].away_score, 5)
 
     def test_check_if_match_ongoing(self):
         # Test to check if any team exists
@@ -43,3 +42,41 @@ class TestScoreBoard(TestCase):
             leaderboard.update_score(home_team, away_team, 10, 5)
 
         self.assertTrue(f"There is no ongoing game between {home_team} and {away_team}\n" in str(context.exception))
+
+    def test_finish_game(self):
+        leaderboard = Scoreboard()
+        home_team = "Germany"
+        away_team = "France"
+        match_key = f"{home_team}-{away_team}"
+        leaderboard.start_game(home_team, away_team)
+        leaderboard.update_score(home_team, away_team, 2, 2)
+        home_team_2 = "Spain"
+        away_team_2 = "Brazil"
+        match_key_2 = f"{home_team_2}-{away_team_2}"
+        leaderboard.start_game(home_team_2, away_team_2)
+        leaderboard.update_score(home_team, away_team, 10, 2)
+        self.assertEqual(leaderboard.games[match_key].home_team, "Germany")
+        self.assertEqual(leaderboard.games[match_key_2].home_team, "Spain")
+        leaderboard.finish_game(home_team, away_team)
+        self.assertNotIn(match_key, leaderboard.games.keys())
+
+    def test_get_games_summary(self):
+        """"We will first insert sample given games in leaderboard and then update their score"""
+        # a. Mexico 0 - Canada 5
+        # b. Spain 10 - Brazil 2
+        # c. Germany 2 - France 2
+        # d. Uruguay 6 - Italy 6
+        # e. Argentina 3 - Australia 1
+        leaderboard = Scoreboard()
+        leaderboard.start_game("Mexico", "Canada")
+        leaderboard.start_game("Spain", "Brazil")
+        leaderboard.start_game("Germany", "France")
+        leaderboard.start_game("Uruguay", "Italy")
+        leaderboard.start_game("Argentina", "Australia")
+
+        leaderboard.update_score("Mexico", "Canada", 0, 5)
+        leaderboard.update_score("Spain", "Brazil", 10, 2)
+        leaderboard.update_score("Germany", "France", 2, 2)
+        leaderboard.update_score("Uruguay", "Italy", 6, 6)
+        leaderboard.update_score("Argentina", "Australia", 3, 1)
+        pass
