ddd841cb84d065c2ef2352535fbfd0a6dfa164cc
Vivek Srivastava <435616+vivekfe@users.noreply.github.com>
2023-01-15 10:09:14 +0100

    - Adding Pytest package for leveraging Pycharm's unittest framework - adding game class which can handle operations on invividual games - writing tests for initializing game instance and updating the score of individual games

---
+++ scorecardlib/game.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+
+
+class Game:
+    def __init__(self, home_team: str, away_team: str):
+        """"Constructor which starts the game with the names of the teams and initializes with 0,0 score"""
+        self.home_team = home_team
+        self.away_team = away_team
+        self.home_score = 0
+        self.away_score = 0
+        self.start_time = datetime.now()
+        self.end_time = None
+
+    def update_score(self, home_score: int, away_score: int):
+        """"Method to update the score of an individual match"""
+        self.home_score = home_score
+        self.away_score = away_score
---
+++ tests/Test_GameClass.py
@@ -0,0 +1,32 @@
+from unittest import TestCase
+from scorecardlib.game import Game
+"""Sample table provided as an input and can be used for writing tests as well"""
+
+# a. Mexico 0 - Canada 5
+# b. Spain 10 - Brazil 2
+# c. Germany 2 - France 2
+# d. Uruguay 6 - Italy 6
+# e. Argentina 3 - Australia 1
+
+
+class TestGame(TestCase):
+    def test_game_init(self):
+        game = Game(home_team="Mexico", away_team="Canada")
+        self.assertEqual(game.home_team, "Mexico")
+        self.assertEqual(game.away_team, "Canada")
+        self.assertEqual(game.home_score, 0)
+        self.assertEqual(game.away_score, 0)
+        self.assertIsNotNone(game.start_time)
+        self.assertIsNone(game.end_time)
+
+    def test_update_score(self):
+        game = Game(home_team="Mexico", away_team="Canada")
+        game.update_score(home_score=0, away_score=5)
+        self.assertEqual(game.home_team, "Mexico")
+        self.assertEqual(game.away_team, "Canada")
+        self.assertEqual(game.home_score, 0)
+        self.assertEqual(game.away_score, 5)
+        self.assertIsNotNone(game.start_time)
+        self.assertIsNone(game.end_time)
+
+
